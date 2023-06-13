c205b91ed9d80fa352caa1216c9d3047fd51dce9
Omar Alshafei <omaralshafei02@gmail.com>
2023-06-13 00:56:14 -0400

    ;

--- Assignment 1/App.java
+++ Assignment 1/App.java
@@ -1,7 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
     TwoFourTree tft = new TwoFourTree();
-    tft.addValue(2);
-
+    tft.addValue(23);
+    tft.addValue(24);
+    tft.printInOrder();
+     tft.addValue(2);   
+    tft.addValue(1);    
+     tft.addValue(42);   
+    tft.addValue(31);
+    tft.addValue(2);    
+    tft.printInOrder();
     }
 }
