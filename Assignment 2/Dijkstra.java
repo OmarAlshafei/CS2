fccf1c55054d55a2eec8a017d077ffef0895dbf6
Omar Alshafei <omaralshafei@Omars-Air.lan>
2023-07-11 18:51:21 -0400

    l

--- Assignment 2/Dijkstra.java
+++ Assignment 2/Dijkstra.java
@@ -1,71 +1,58 @@
+// An adjacency matrix method using a Set for vertices,and a two-dimensional array of Booleans to store the edges.
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-import java.lang.*; 
-import java.io.*; 
+
 public class Dijkstra {
     private Set<Integer> vertices;
-    private Map<VertexInfo, Boolean> adjacencyMatrix;
-
+    private int[][] adjacencyMatrix;
+    
     public Dijkstra() {
         vertices = new HashSet<>();
-        adjacencyMatrix = new HashMap<>();
+        adjacencyMatrix = new int[0][0];
     }
 
     public void addVertex(int vertex) {
         vertices.add(vertex);
+        resizeMatrix();
     }
 
     public void addEdge(int source, int destination, int weight) {
-        if (!vertices.contains(source) || !vertices.contains(destination))
-            throw new IllegalArgumentException("One or more vertices do not exist.");
-
-        VertexInfo edge = new VertexInfo(source, destination, weight);
-        adjacencyMatrix.put(edge, true);
+        if (!vertices.contains(source))
+            vertices.add(source);
+            
+        if(!vertices.contains(destination)){
+            vertices.add(destination);
+        }
+            
+        adjacencyMatrix[source][destination] = weight;
+        adjacencyMatrix[destination][source] = weight;
     }
 
-    public boolean hasEdge(int source, int destination, int weight) {
-        VertexInfo edge = new VertexInfo(source, destination, weight);
-        return adjacencyMatrix.getOrDefault(edge, false);
+    public void resizeMatrix() {
+        int size = vertices.size();
+        int[][] newMatrix = new int[size][size];
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                newMatrix[i][j] = adjacencyMatrix[i][j];
+            }
+        }
+
+        adjacencyMatrix = newMatrix;
     }
 
     public void printGraph() {
         for (int vertex : vertices) {
-            for (VertexInfo edge : adjacencyMatrix.keySet()) {
-                if (edge.source == vertex) {
-                    System.out.println(vertex + " " + edge.destination + " " + edge.weight + "");
+            System.out.print("Vertex " + vertex + " is connected to: ");
+            for (int i = 0; i < adjacencyMatrix.length; i++) {
+                if (adjacencyMatrix[vertex][i] != -1) {
+                    System.out.print(i + " ");
                 }
             }
-        }
-    }
-
-    private class VertexInfo {
-        private int source;
-        private int destination;
-        private int weight;
-        private int parent = -1;
-        private int defaultWeight = 999999;
-        
-        public VertexInfo(int source, int destination, int weight) {
-            this.source = source;
-            this.destination = destination;
-            this.weight = weight;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-
-            VertexInfo other = (VertexInfo) obj;
-            return (source == other.source && destination == other.destination && weight == other.weight)
-                    || (source == other.destination && destination == other.source);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(source, destination, weight) + Objects.hash(destination, source, weight);
+            System.out.println();
         }
     }
 
@@ -74,27 +61,19 @@ public class Dijkstra {
 
         String file = "cop3503-asn2-input.txt";
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+        Scanner sc = new Scanner(new FileReader(file));
 
         String line;
         int numVertices = 0;
         int sourceVertex = 0;
         int numEdges = 0;
-
-        // Read the file line by line
-        while ((line = reader.readLine()) != null) {
-            line = line.trim();
-
-            String[] parts = line.split(" ");
-
-            if (numVertices == 0) {
-                numVertices = Integer.parseInt(parts[0]);
-            } 
-            else if (sourceVertex == 0) {
-                sourceVertex = Integer.parseInt(parts[0]);
-            } 
-            else if (numEdges == 0) {
-                numEdges = Integer.parseInt(parts[0]);
+        
+        if(parse
+        numVertices = sc.;
+        
+        sourceVertex = sc.nextInt();
+        
+        numEdges = sc.nextInt();
             } 
             else {
                 int vertex = Integer.parseInt(parts[0]);
