404be945a2eb1087b831603988b89940bd7a31e2
Omar Alshafei <omaralshafei02@gmail.com>
2023-07-11 21:27:48 -0400

    l

--- Assignment 2/Dijkstra.java
+++ Assignment 2/Dijkstra.java
@@ -1,6 +1,3 @@
-// An adjacency matrix method using a Set for vertices,and a two-dimensional array of Booleans to store the edges.
-
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -8,15 +5,23 @@ import java.util.*;
 public class Dijkstra {
     private Set<Integer> vertices;
     private int[][] adjacencyMatrix;
-    
-    public Dijkstra() {
+    public int sourceVertex;
+    public int numVertices;
+
+
+    public Dijkstra(int sourceVertex, int numVertices) {
+        this.sourceVertex = sourceVertex;
+        this.numVertices = numVertices;
         vertices = new HashSet<>();
-        adjacencyMatrix = new int[0][0];
+        adjacencyMatrix = new int[numVertices + 1][numVertices + 1];
+        
+        for(int [] arr : adjacencyMatrix){
+            Arrays.fill(arr, 0);
+        }
     }
 
     public void addVertex(int vertex) {
         vertices.add(vertex);
-        resizeMatrix();
     }
 
     public void addEdge(int source, int destination, int weight) {
@@ -44,48 +49,85 @@ public class Dijkstra {
         adjacencyMatrix = newMatrix;
     }
 
-    public void printGraph() {
-        for (int vertex : vertices) {
-            System.out.print("Vertex " + vertex + " is connected to: ");
-            for (int i = 0; i < adjacencyMatrix.length; i++) {
-                if (adjacencyMatrix[vertex][i] != -1) {
-                    System.out.print(i + " ");
+    
+    public void dijkstraAlgorithm() {
+        int[] distance = new int[numVertices + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+    
+        int[] parent = new int[numVertices + 1];
+        Arrays.fill(parent, -1);
+    
+        Set<Integer> visited = new HashSet<>();
+    
+        distance[sourceVertex] = 0;
+    
+        while (visited.size() < vertices.size()) {
+            int currentVertex = -1;
+            int minDistance = Integer.MAX_VALUE;
+    
+            for (int vertex : vertices) {
+                if (!visited.contains(vertex) && distance[vertex] < minDistance) {
+                    currentVertex = vertex;
+                    minDistance = distance[vertex];
                 }
             }
-            System.out.println();
+            
+            visited.add(currentVertex);
+    
+            for (int neighbor : vertices) {
+                if (!visited.contains(neighbor) && adjacencyMatrix[currentVertex][neighbor] != 0) {
+                    int newDistance = distance[currentVertex] + adjacencyMatrix[currentVertex][neighbor];
+                    if (newDistance < distance[neighbor]) {
+                        distance[neighbor] = newDistance;
+                        parent[neighbor] = currentVertex;
+                    }
+                }
+            }
         }
+        
+        distance[sourceVertex] = -1;
+        System.out.println(numVertices);
+        for (int vertex : vertices)
+            System.out.println(vertex + " " +distance[vertex]+ " " + parent[vertex]);
     }
-
+    
     public static void main(String[] args) throws IOException {
-        Dijkstra graph = new Dijkstra();
 
         String file = "cop3503-asn2-input.txt";
 
         Scanner sc = new Scanner(new FileReader(file));
-
-        String line;
-        int numVertices = 0;
-        int sourceVertex = 0;
-        int numEdges = 0;
         
-        if(parse
-        numVertices = sc.;
+        List<Integer> input  = new ArrayList<>();
         
-        sourceVertex = sc.nextInt();
+        int vertex = -1;
+        int destinationVertex = -1;
+        int weight = -1;
         
-        numEdges = sc.nextInt();
-            } 
-            else {
-                int vertex = Integer.parseInt(parts[0]);
-                int destinationVertex = Integer.parseInt(parts[1]);
-                int weight = Integer.parseInt(parts[2]);
-
-                graph.addVertex(vertex);
-                graph.addVertex(destinationVertex);
-                graph.addEdge(vertex, destinationVertex, weight);
-            }
+        while(sc.hasNext()){
+        if (sc.hasNextInt())
+            input.add(sc.nextInt());
+        else
+            sc.next();
+        }        
+        
+        int i = 0;
+        int numVertices = input.get(i++);
+        int sourceVertex = input.get(i++);
+        int numEdges = input.get(i++);
+        
+        Dijkstra graph = new Dijkstra(sourceVertex, numVertices);
+        
+        while (input.size() > i) {
+            vertex = input.get(i++);
+            destinationVertex = input.get(i++);
+            weight = input.get(i++);
+            graph.addVertex(vertex);
+            graph.addVertex(destinationVertex);
+            graph.addEdge(vertex, destinationVertex, weight);
         }
-        reader.close();
-        graph.printGraph();
+
+        sc.close();
+        graph.dijkstraAlgorithm();
+
     }
 }
